b706b4cb34405e6dc9c63e4b5749f032131c4323
aditisah <sditi.sah1@gmail.com>
2023-06-02 12:55:01 +0530

    menu button touch issue resolved

--- public/js/main.js
+++ public/js/main.js
@@ -17,28 +17,36 @@ document.addEventListener("DOMContentLoaded", ()=> {
             }
           })
           const menuBackground = document.getElementById('menu-overlay')
-          menu.addEventListener("touchstart",(e)=>{
-            e.preventDefault()
-            if(window.innerWidth<=768){
-              if (touchstartX === undefined) {
-    touchstartX = e.touches[0].clientX;
-    menuItems.classList.remove("hidden");
-    menuButton.classList.add("hidden");
-    menuBackground.classList.remove("hidden");
-    // menuItems.style.position = 'absolute'
-    // menuItems.style.top = '46px'
-    // menuItems.style.left = '81.2%'
-    // menuItems.style.transform = 'translateX(-50%)'
-    // menuItems.style.width = 'calc(100vw - 10rem)'
-    // menuItems.style.backgroundColor = 'white'
-    // menuItems.style.zIndex = '20'
-  } else {
-    touchstartX = undefined;
-    menuItems.classList.add("hidden");
-    menuButton.classList.remove("hidden");
-  }
-            }
-          })
+    //       menuButton.addEventListener('click', ()=>{
+    //         menuItems.classList.remove("hidden");
+    //  menuButton.classList.add("hidden");
+    //  menuBackground.classList.remove("hidden");
+    //       })
+           menuButton.addEventListener("touchstart",(e)=>{
+             e.preventDefault()
+             if(window.innerWidth<=768){
+    //            if (touchstartX === undefined) {
+    //  touchstartX = e.touches[0].clientX;
+     menuItems.classList.remove("hidden");
+     menuButton.classList.add("hidden");
+     menuBackground.classList.remove("hidden");
+     // menuItems.style.position = 'absolute'
+     // menuItems.style.top = '46px'
+     // menuItems.style.left = '81.2%'
+     // menuItems.style.transform = 'translateX(-50%)'
+     // menuItems.style.width = 'calc(100vw - 10rem)'
+     // menuItems.style.backgroundColor = 'white'
+     // menuItems.style.zIndex = '20'
+               //}
+             }
+            })
+  // } else {
+  //   touchstartX = undefined;
+  //   menuItems.classList.add("hidden");
+  //   menuButton.classList.remove("hidden");
+  // }
+  //           }
+  //         })
           const menuOverlayCloseButton = document.getElementById('menu-close-button')
    const menuCloseButton = ()=>{
     menuBackground.classList.add('hidden')
